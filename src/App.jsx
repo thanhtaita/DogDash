5b9adf49daf5574067fc020cbf55581fd980f369
Umair Haque <umairsmsey@gmail.com>
2023-03-21 22:49:02 -0500

    streatched goals

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,77 @@
-import {useEffect, useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
+import viteLogo from "/vite.svg";
 import CoinInfo from "./components/coinInfo";
 import { Input } from "semantic-ui-react";
-
+import SideNav from "./Components/sideNav";
+import "./App.css";
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
-
 function App() {
-  const [list, setList] = useState(null)
+  const [list, setList] = useState(null);
   const [filteredResults, setFilteredResults] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
-  const searchItems = searchValue => {
+  const searchItems = (searchValue) => {
     setSearchInput(searchValue);
     if (searchValue !== "") {
-      const filteredData = Object.keys(list.Data).filter((item) => 
+      const filteredData = Object.keys(list.Data).filter((item) =>
         Object.values(item)
           .join("")
           .toLowerCase()
           .includes(searchValue.toLowerCase())
-      )
+      );
       setFilteredResults(filteredData);
     } else {
       setFilteredResults(Object.keys(list.Data));
     }
-
   };
 
-
   useEffect(() => {
-
     const fetchAllCoinData = async () => {
-      
       const response = await fetch(
-        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" 
-    + API_KEY
-
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
       );
       const json = await response.json();
-        console.log(json);
+      console.log(json);
       setList(json);
     };
 
-
     fetchAllCoinData().catch(console.error);
-
   }, []);
 
-
-  
-
-
   return (
-<div className="whole-page">
-
-  <h1>My Crypto List</h1>
-  <input
-    type="text"
-    placeholder="Search..."
-    onChange={(inputString) => searchItems(inputString.target.value)}
-  />
+    <div className="whole-page">
+      <SideNav />
+      <h1>My Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
       <ul>
-      {searchInput.length > 0
-      ? filteredResults.map((coin) => 
-            list.Data[coin].PlatformType === "blockchain" ? 
-            <CoinInfo
-              image={list.Data[coin].ImageUrl}
-              name={list.Data[coin].FullName}
-              symbol={list.Data[coin].Symbol}
-            />
-            : null
-        )
-      : list && Object.entries(list.Data).map(([coin]) => 
-            list.Data[coin].PlatformType === "blockchain" ? 
-            <CoinInfo
-              image={list.Data[coin].ImageUrl}
-              name={list.Data[coin].FullName}
-              symbol={list.Data[coin].Symbol}
-            />
-            : null
-        )}
-
+        {searchInput.length > 0
+          ? filteredResults.map((coin) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
+            )
+          : list &&
+            Object.entries(list.Data).map(([coin]) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
+            )}
       </ul>
-</div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/sideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CryptoScam from "./cryptoScam";
+
+const SideNav = () => (
+  <div className="sidenav">
+    <h1>here is my side nav bar</h1>
+    <CryptoScam />
+  </div>
+);
+
+export default SideNav;
